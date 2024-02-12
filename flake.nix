cb685ded3ea9bf5e6798364556373abf8cbfda48
Robin Appelman <robin@icewind.nl>
2024-02-12 19:35:39 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    inherit (flake-utils.lib) eachDefaultSystem eachSystem;
+  in eachDefaultSystem (system: let
+      pkgs = (import nixpkgs) {
+        inherit system;
+      };
+    in rec {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [cargo rustc clippy bacon cargo-edit cargo-msrv];
+      };
+    });
+}
